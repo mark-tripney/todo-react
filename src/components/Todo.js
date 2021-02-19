04537c4aaaf7299c86e9ae48b76bf0d9ad51c36c
Mark Tripney <mrktrpny@gmail.com>
2021-02-19 17:06:37 +0100

    Add 'toggle complete' (on click) functionality

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -1,8 +1,17 @@
-import React from 'react';
+import React, {useState} from 'react';
 
 export default function Todo(props) {
-  const {id, name, completed} = props;
+  const [isComplete, setIsComplete] = useState(props.completed);
+  console.log(isComplete);
+  const {id, name} = props;
+
+  function toggleCompleted(e) {
+    setIsComplete(!isComplete);
+    console.log(e.target);
+  }
+
   return (
-      <li id={id} className={completed ? "completed" : ""}>{name}</li>
+      <li id={id} className={isComplete ? "completed" : ""}
+          onClick={toggleCompleted}>{name}</li>
   );
 }
