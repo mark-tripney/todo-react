1e8fee062d894473890c72db86f3f03be07d71c0
Mark Tripney <mrktrpny@gmail.com>
2021-02-19 11:24:18 +0100

    Simplify completion className ('complete-...' to 'completed')

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -3,6 +3,6 @@ import React from 'react';
 export default function Todo(props) {
   const {id, name, completed} = props;
   return (
-      <li id={id} className={`complete-${completed}`}>{name}</li>
+      <li id={id} className={completed ? "completed" : ""}>{name}</li>
   );
 }
